9bf409df0eb65186d835d852f182f62c157c5107
Nanthesh <nanthesh8kannan@gmail.com>
2024-05-31 13:12:25 -0400

    react app created

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+const App = () => {
+  return (
+    <div className='container'>
+      <h1>Nanthesh Kannan</h1>
+      <h2>C0896092</h2>
+    </div>
+  )
+}
+
+export default App;
